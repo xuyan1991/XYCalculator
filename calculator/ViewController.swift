69b8219e44ce6581d6b54224a046482c4fd1fd27
jhbank <xuyan.1991@foxmail.com>
2016-03-04 14:04:17 +0800

    modify sth

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,34 +9,68 @@
 import UIKit
 import Material
 import Foundation
+import SnapKit
 
 
 class ViewController: UIViewController {
 
-    private var resultLabel:UILabel?
+    var titleLabel: UILabel = UILabel()
+    var detailLabel: UILabel = UILabel()
+    let resultView: MaterialView = MaterialView()
+    private var beforeOp: String = ""
     private var typingFlag: Bool = false
-    private var buttonView:MaterialView?
+    private var opFlag: Bool = false
     private var buttonList: [UIView] = [UIView]()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = MaterialColor.white
+        titleLabel.text = "INPUT"
+        titleLabel.textColor = MaterialColor.blue.darken1
+        titleLabel.textAlignment = .Right
+        titleLabel.font = RobotoFont.mediumWithSize(20)
+        titleLabel.alpha = 1
+        detailLabel.text = "RESULT"
+        detailLabel.textAlignment = .Right
+        detailLabel.font = RobotoFont.mediumWithSize(40)
+        detailLabel.alpha = 1
+
+        let buttonView = MaterialView()
         
-        var resultView:MaterialView
-        resultView = MaterialView(frame: CGRectMake(10, 30, UIScreen.mainScreen().bounds.width - 20, 88))
-        resultView.depth = .Depth3
-        resultLabel = UILabel(frame: CGRectMake(0, 0, resultView.bounds.width - 20, 44))
-        resultLabel!.backgroundColor = MaterialColor.white
-        resultLabel!.text = "结果"
-        resultLabel!.textColor = MaterialColor.blue.accent3
-        resultLabel!.textAlignment = .Right
-        resultLabel!.font = RobotoFont.medium
-        resultView.addSubview(resultLabel!)
-        buttonView = MaterialView(frame: CGRectMake(0, resultView.frame.maxY + 30, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height - 200))
-        
-        let buttonTitles: Array<String> = [ "9", "8", "7", "/",
-                                            "6", "5", "4", "*",
-                                            "3", "2", "1", "+",
+        let buttonTitles: Array<String> = [ "7", "8", "9", "/",
+                                            "4", "5", "6", "*",
+                                            "1", "2", "3", "+",
                                             ".", "0", "=", "-"]
+        resultView.depth = .Depth3
+        resultView.addSubview(titleLabel)
+        resultView.addSubview(detailLabel)
+        self.view.addSubview(buttonView)
+        self.view.addSubview(resultView)
+        resultView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view).offset(20)
+            make.left.equalTo(self.view).offset(20)
+            make.right.equalTo(self.view).offset(-20)
+            make.height.equalTo(152)
+            
+        }
+        titleLabel.snp_makeConstraints{ (make) ->Void in
+            make.top.equalTo(resultView).offset(10)
+            make.left.equalTo(resultView).offset(10)
+            make.right.equalTo(resultView).offset(-10)
+            make.height.equalTo(44)
+        }
+        detailLabel.snp_makeConstraints{ (make) ->Void in
+            make.top.equalTo(titleLabel.snp_bottom)
+            make.left.equalTo(resultView).offset(10)
+            make.bottom.equalTo(resultView).offset(-10)
+            make.right.equalTo(resultView).offset(-10)
+        }
+
+        buttonView.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(resultView.snp_bottom).offset(10)
+            make.left.equalTo(self.view).offset(10)
+            make.bottom.equalTo(self.view).offset(-20)
+            make.right.equalTo(self.view).offset(-10)
+        }
         for index in 0...15{
             
             let button: RaisedButton = RaisedButton()
@@ -49,7 +83,7 @@ class ViewController: UIViewController {
             button.depth = .Depth2
             button.backgroundColor = MaterialColor.grey.base
             button.addTarget(self,action:Selector("tapped:"),forControlEvents:.TouchUpInside)
-
+            
             switch index{
             case 14:
                 button.backgroundColor = MaterialColor.red.darken1
@@ -60,27 +94,17 @@ class ViewController: UIViewController {
             default: break
             }
             buttonList.append(button)
+            buttonView.addSubview(button)
             
             
-   
-            buttonView!.addSubview(button)
-            
-           
         }
-        buttonView!.grid.axis.direction = .None
-        buttonView!.grid.spacing = 8
-        buttonView!.grid.axis.inherited = false
-        buttonView!.grid.views = buttonList
-        buttonView!.grid.axis.columns = 12
-        buttonView!.grid.axis.rows = 12
-        buttonView!.grid.contentInsetPreset = .Square3
-        //buttonView.
-        self.view.addSubview(resultView)
-        self.view.addSubview(buttonView!)
-
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        buttonView.grid.axis.direction = .None
+        buttonView.grid.spacing = 8
+        buttonView.grid.axis.inherited = false
+        buttonView.grid.views = buttonList
+        buttonView.grid.axis.columns = 12
+        buttonView.grid.axis.rows = 12
+        buttonView.grid.contentInsetPreset = .Square3
     }
 
     override func didReceiveMemoryWarning() {
@@ -88,89 +112,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     func tapped(button:UIButton){
-        if(typingFlag){
-            resultLabel!.text! = "\(resultLabel!.text!)\(button.currentTitle!)"
+        print(beforeOp)
+        let ops: [String] = ["*", "+", "-", "/", "."]
+        if ops.contains(beforeOp) && ops.contains(button.currentTitle!){
+            opFlag = true
         }
         else{
-            resultLabel!.text! = "\(button.currentTitle!)"
+            opFlag = false
         }
+        if opFlag == false {
+            if(typingFlag) {
+                titleLabel.text! = "\(titleLabel.text!)\(button.currentTitle!)"
+            }
+            else{
+                titleLabel.text! = "\(button.currentTitle!)"
+            }
+            beforeOp = String(titleLabel.text!.characters.last!)
+        }
+        
+        let cal: CalculatorBrain = CalculatorBrain()
+        let result: Double = cal.eval(titleLabel.text!)
+        detailLabel.text! = "\(result)"
         typingFlag = true
-        
-        
     }
     func bingo(button:UIButton){
-        let cal: CalculatorBrain = CalculatorBrain()
         
-        let result: Double = cal.eval(resultLabel!.text!)
-        resultLabel!.text! = "\(result)"
-       // print(resultLabel!.text! )
-        if(resultLabel!.text! == "1990.09.08="){
-            buttonList.removeFirst()
-            var view:UIView?
-
-            
-            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
-                
-                //这里写需要大量时间的代码
-                print("这里写需要大量时间的代码")
-                
-                dispatch_async(dispatch_get_main_queue(), {
-                    
-                    //这里返回主线程，写需要主线程执行的代码
-                    print("这里返回主线程，写需要主线程执行的代码")
-                })
-            })
-            
-            if(true)
-            {
-                view = buttonView!.subviews[0]
-                
-                view!.removeFromSuperview()
-                //delay(2.0, task: <#T##() -> ()#>)
-            }
-            buttonView!.grid.views = buttonList
-           // buttonView?.grid
-        }
-
+        titleLabel.text = ""
         typingFlag = false
-        
-        
     }
     
 
-    typealias Task = (cancel : Bool) -> ()
-    func delay(time:NSTimeInterval, task:()->()) ->  Task? {
-        func dispatch_later(block:()->()) {
-            dispatch_after(
-                dispatch_time(
-                    DISPATCH_TIME_NOW,
-                    Int64(time * Double(NSEC_PER_SEC))),
-                dispatch_get_main_queue(),
-                block)
-        }
-        var closure: dispatch_block_t? = task
-        var result: Task?
-        let delayedClosure: Task = {
-            cancel in
-            if let internalClosure = closure {
-                if (cancel == false) {
-                    dispatch_async(dispatch_get_main_queue(), internalClosure);
-                }
-            }
-            closure = nil
-            result = nil
-        }
-        result = delayedClosure
-        dispatch_later {
-            if let delayedClosure = result {
-                delayedClosure(cancel: false)
-            }
-        }
-        return result;
-    }
-    func cancel(task:Task?) {
-        task?(cancel: true)
-    }
+
 
 }
+
 
